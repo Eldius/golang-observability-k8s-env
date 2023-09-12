d74ad81985cf8d692a290bc61bb9a41fa75894cd
Eldio Santos <eldio.santos@bemobi.com>
2023-09-12 18:41:38 -0300

    Some setup improvements...

---
+++ terraform/index-patterns/main.tf
@@ -0,0 +1,22 @@
+
+terraform {
+  required_providers {
+    # elasticsearch = {
+    #   source = "phillbaker/elasticsearch"
+    #   #version = "2.0.7"
+    # }
+    opensearch = {
+      source = "opensearch-project/opensearch"
+      #version = "2.0.0"
+    }
+  }
+
+}
+
+provider "opensearch" {
+  # Configuration options
+  url="https://192.168.100.195:9200"
+  username="admin"
+  password="admin"
+  insecure = true
+}
--- terraform/log-indexes/main.tf
+++ terraform/log-indexes/main.tf
@@ -1,10 +1,10 @@
 
 terraform {
   required_providers {
-    elasticsearch = {
-      source = "phillbaker/elasticsearch"
-      #version = "2.0.7"
-    }
+    # elasticsearch = {
+    #   source = "phillbaker/elasticsearch"
+    #   #version = "2.0.7"
+    # }
     opensearch = {
       source = "opensearch-project/opensearch"
       #version = "2.0.0"
@@ -15,9 +15,9 @@ terraform {
 
 provider "opensearch" {
   # Configuration options
-  url="https://192.168.100.195:9200"
-  username="admin"
-  password="admin"
+  url      = "https://192.168.100.195:9200"
+  username = "admin"
+  password = "admin"
   insecure = true
 }
 
@@ -26,5 +26,10 @@ resource "opensearch_index" "logs" {
   name               = "application-logs-00001"
   number_of_shards   = 1
   number_of_replicas = 0
+  aliases            = jsonencode({
+    "application-logs" = {
+      "is_write_index" = true
+    }
+  })
   mappings           = file("${path.module}/mappings/log-index.json")
 }
